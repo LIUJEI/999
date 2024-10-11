d0e885daa4bf52f2969d8c167ba6840f1bb754d4
琉 芸 <3430477977@qq.com>
2024-10-11 01:24:13 +0000

    Update 短剧天堂.js

--- TV/短剧天堂.js
+++ TV/短剧天堂.js
@@ -16,8 +16,8 @@ var rule = {
     quickSearch: 1,
     filterable: 1,
 
-    class_name: '✨逆袭短剧&✨都市短剧&✨神医短剧&✨重生短剧&✨玄幻短剧&✨萌宝短剧',
-    class_url: '1&20&25&30&32&28',
+    class_name: '✨逆袭短剧&✨都市短剧&✨神医短剧&✨重生短剧&✨虐恋短剧&✨萌宝短剧',
+    class_url: '1&20&25&30&3&28',
     filter_def: {
         1: {cateId: '1'},
         20: {cateId: '20'},
